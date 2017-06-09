ec498dcbbab26a99f7d8dd3bbd67d6d18d834615
Will Smith <wsmith@scottlogic.co.uk>
2017-06-09 14:08:17 +0100

    Minor tweaks

--- public/js/components/court.js
+++ public/js/components/court.js
@@ -4,22 +4,20 @@ import courtHtml from '../../views/components/court.html'
 
 export default class Court {
     constructor() {
-        this.scale = '0 0 100 125';
+        this.scale = '9 30 100 125';
         this.width = 100;
         this.height = 125;
     }
 
     render(node) {
-      node.append('svg')
-        .attr('width', this.width)
-        .attr('height', this.height)
-        .attr('viewBox', this.scale)
+
+      const svgNode = node.append('svg')
+        .attr('viewBox', '9.2 30 83 40')
         .html(courtHtml);
     }
 
-    setScale (x, y) {
-        this.width = x;
-        this.height = y;
-        this.scale = '0 0 ' + x + ' ' + y;
+    setScale (width, height) {
+        this.width = width;
+        this.height = height;
     }
 }
